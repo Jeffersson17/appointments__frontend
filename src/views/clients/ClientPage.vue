c4693a0c4139a46769ac2fb34df994b974e5641a
Jeffersson Barros <barrosjefferson407@gmail.com>
2026-04-05 17:15:33 -0300

    clients views

---
+++ src/views/clients/ClientPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TableClients from './TableClients.vue';
+</script>
+
+<template>
+  <CRow>
+    <CCol class="mb-4">
+      <CCard>
+        <CCardHeader> Line Chart </CCardHeader>
+        <CCardBody>
+          <TableClients />
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+</template>
---
+++ src/views/clients/TableClients.vue
@@ -0,0 +1,42 @@
+<script setup>
+  const columns = [
+    {
+      key: 'name',
+      label: 'Nome',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'heading_1',
+      label: 'Heading',
+      _props: { scope: 'col' },
+    },
+    {
+      key: 'heading_2',
+      label: 'Heading',
+      _props: { scope: 'col' },
+    },
+  ]
+  const items = [
+    {
+      name: 'Mark',
+      heading_1: 'Otto',
+      heading_2: '@mdo',
+      _cellProps: { id: { scope: 'row' } },
+    },
+    {
+      name: 'Jacob',
+      heading_1: 'Thornton',
+      heading_2: '@fat',
+      _cellProps: { id: { scope: 'row' } },
+    },
+    {
+      name: 'Larry the Bird',
+      heading_2: '@twitter',
+      _cellProps: { id: { scope: 'row' }, class: { colSpan: 2 } },
+    },
+  ]
+</script>
+
+<template>
+  <CTable :columns="columns" :items="items" />
+</template>
