b5ac8bb1416d017570c1cb0f95f593b8134dd76c
Jeffersson Barros <barrosjefferson407@gmail.com>
2026-04-07 22:08:26 -0300

    fix login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -2,54 +2,41 @@
   <div class="wrapper min-vh-100 d-flex flex-row align-items-center">
     <CContainer>
       <CRow class="justify-content-center">
-        <CCol :md="8">
-          <CCardGroup>
-            <CCard class="p-4">
-              <CCardBody>
-                <CForm>
-                  <h1>Login</h1>
-                  <p class="text-body-secondary">Sign In to your account</p>
-                  <CInputGroup class="mb-3">
-                    <CInputGroupText>
-                      <CIcon icon="cil-user" />
-                    </CInputGroupText>
-                    <CFormInput placeholder="Username" autocomplete="username" />
-                  </CInputGroup>
-                  <CInputGroup class="mb-4">
-                    <CInputGroupText>
-                      <CIcon icon="cil-lock-locked" />
-                    </CInputGroupText>
-                    <CFormInput
-                      type="password"
-                      placeholder="Password"
-                      autocomplete="current-password"
-                    />
-                  </CInputGroup>
-                  <CRow>
-                    <CCol :xs="6">
-                      <CButton color="primary" class="px-4"> Login </CButton>
-                    </CCol>
-                    <CCol :xs="6" class="text-right">
-                      <CButton color="link" class="px-0"> Forgot password? </CButton>
-                    </CCol>
-                  </CRow>
-                </CForm>
-              </CCardBody>
-            </CCard>
-            <CCard class="text-white bg-primary py-5" style="width: 44%">
-              <CCardBody class="text-center">
-                <div>
-                  <h2>Sign up</h2>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua.
-                  </p>
-                  <CButton color="light" variant="outline" class="mt-3"> Register Now! </CButton>
-                  <CButton color="danger" @click="forceError"> Testar erro 500 </CButton>
-                </div>
-              </CCardBody>
-            </CCard>
-          </CCardGroup>
+        <CCol :md="6" :lg="5">
+          <CCard class="p-4">
+            <CCardBody>
+              <CForm>
+                <h1>Login</h1>
+                <p class="text-body-secondary">Insira suas credenciais</p>
+
+                <CInputGroup class="mb-3">
+                  <CInputGroupText>
+                    <CIcon icon="cil-envelope-closed" />
+                  </CInputGroupText>
+                  <CFormInput placeholder="Email" />
+                </CInputGroup>
+
+                <CInputGroup class="mb-4">
+                  <CInputGroupText>
+                    <CIcon icon="cil-lock-locked" />
+                  </CInputGroupText>
+                  <CFormInput type="password" placeholder="Senha" />
+                </CInputGroup>
+
+                <CRow>
+                  <CCol :xs="6">
+                    <CButton color="primary" class="px-4" type="button" @click="login">
+                      Login
+                    </CButton>
+                  </CCol>
+
+                  <CCol :xs="6" class="text-end">
+                    <CButton color="link" class="px-0"> Esqueceu a senha? </CButton>
+                  </CCol>
+                </CRow>
+              </CForm>
+            </CCardBody>
+          </CCard>
         </CCol>
       </CRow>
     </CContainer>
@@ -57,7 +44,11 @@
 </template>
 
 <script setup>
-const forceError = () => {
-  throw new Error('Erro forçado para testar a página 500')
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const login = () => {
+  router.push({ name: 'Dashboard' })
 }
 </script>
