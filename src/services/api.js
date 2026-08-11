0448bb1683cbca1d1fdff55d70020470bdbcf2d3
Jeffersson Barros <barrosjefferson407@gmail.com>
2026-08-11 20:27:15 -0300

    add interceptors to reponse

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,39 @@
-import axios from "axios";
+import axios from 'axios'
+import router from '@/router'
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/",
-});
+  baseURL: 'http://localhost:8000/',
+})
 
+// Interceptador para adicionar o token de autenticação nas requisições
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("access");
-        if (token) {
-            config.headers["Authorization"] = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem('access')
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`
     }
-);
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
 
-export default api;
+// Interceptador de resposta para lidar com erros de autenticação 401
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('access')
+      localStorage.removeItem('refresh')
+
+      if (router.currentRoute.value.name !== 'Login') {
+        router.push({ name: 'Login' })
+      }
+    }
+
+    return Promise.reject(error)
+  },
+)
+
+export default api
